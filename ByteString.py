865a00055f2be61e96329b620a886a0105ded30b
cyber-hamster <cyber_hamster@hotmail.com>
2016-11-19 16:57:26 -0600

    Add files via upload
    Full functionality is present now.

--- ByteString.py
+++ ByteString.py
@@ -1,5 +1,33 @@
-import sys
+#!/usr/bin/python
+__author__= 'OG'
 
-s = "How now Brown Cow"
-print(sys.getsizeof(s))
+# Pads a string with trailing spaces so length equals a mulitple of 16 Bytes
 
+def add_spaces(s, num):
+    for n in range(need_to_add):
+        s += " "
+    print("New string is: ", s)
+    print("Length = ", get_string_length(s) )
+
+def get_string_length(s):
+    return len(s.encode('utf-8'))
+
+go = True
+while go:
+    s = input("Enter a string: ")
+    b_size = get_string_length(s)
+    if b_size < 16:
+        need_to_add = 16 - b_size
+        add_spaces(s, need_to_add)
+
+    elif b_size % 16 != 0:
+        over_byte = b_size % 16
+        need_to_add = 16 - over_byte
+        add_spaces(s, need_to_add)
+
+    else:
+        print("String is: ", s)
+
+    again = input("Do another? ")
+    if again == "n":
+        go = False
